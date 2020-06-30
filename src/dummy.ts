f1ea2b6bbd6011b07aaf1ba816e575bc66960b7c
iwgx <wahabiputra@gmail.com>
2020-06-30 10:48:30 +0700

    Create Job interface and add sample jobs

---
+++ src/dummy.ts
@@ -0,0 +1,38 @@
+import { Job } from "./interfaces";
+
+export const SAMPLE_JOBS: Job[] = [
+  {
+    id: "f1e47ec6-641c-4b4a-a38a-1a57e6af7c93",
+    type: "Full Time",
+    url:
+      "https://jobs.github.com/positions/f1e47ec6-641c-4b4a-a38a-1a57e6af7c93",
+    created_at: "Mon Jun 29 06:38:28 UTC 2020",
+    company: "InnoGames GmbH",
+    company_url: "https://www.innogames.com",
+    location: "Hamburg",
+    title: "Java Software Developer - Core Team",
+    description:
+      '<p>As <strong>Java Software Developer</strong>, you work closely with our game teams to shape the foundation of our next big hits and contribute as a productive member of an agile development team in all phases of the development lifecycle.</p>\n<p><strong>Your mission:</strong></p>\n<ul>\n<li>Develop features shared across our game backends</li>\n<li>Improve performance-critical components, increase the robustness and scalability of our software</li>\n<li>Improve our libraries and backend services with your knowledge of software architecture and high-quality code</li>\n<li>Maintain and extend our development tools and environment</li>\n</ul>\n<p><strong>Your profile:</strong></p>\n<ul>\n<li>Degree in Computer Sciences, Business Information Systems or equivalent training and experience as IT specialist</li>\n<li>Several years of experience in object-oriented programming in Java with Spring Boot and optimally the Akka framework</li>\n<li>Software development with Spring, the UNIX command line and profound knowledge of SQL, GIT, and Continuous Delivery</li>\n<li>You love to dive deep into other people’s code which is reflected in your Github profile</li>\n<li>Solid experience in using various methods in software design and a strong belief in automated testing to guarantee and improve code quality in compliance with the company standards</li>\n<li>Being a backend geek, you also like to switch to the frontend side with Angular, C# and Unity</li>\n<li>Good English language and overall communication skills</li>\n<li>Proactive and motivated individual with a willingness to learn</li>\n</ul>\n<p><strong>Why join us?</strong></p>\n<ul>\n<li>Be part of a great international team in a healthy and stable-growing company</li>\n<li>Your working device will be the latest MacBook Pro with high-end hardware</li>\n<li>We actively support your further development and give you all the needed resources to evaluate new technologies and participate in open source communities</li>\n<li>We provide additional training budget to spend on attending conferences, taking hard skill courses or improve your soft skills as well as purchasing books</li>\n<li>Our career model is set up equally for an expert in a technical career or a management path</li>\n<li>Use opportunities to make your code open source and show your achievements at international conferences</li>\n<li>We offer competitive compensation with benefits like relocation support, company gym, smartphone or tablet, company bikes, rooftop terrace with BBQ and much more</li>\n</ul>\n<p>Excited to start your journey with InnoGames and join our dynamic team as a <strong>Java Software Developer</strong>? We look forward to receiving your application as well as your salary expectations and earliest possible start date through our <a href="https://www.innogames.com/career/detail/job/java-software-developer-core-team/?s=GitHub">online application form</a>. <strong>Cristal Ledesma</strong> would be happy to answer any questions you may have.</p>\n<p><strong>Apply and #stayhome!</strong> Due to the <strong>coronavirus</strong> outbreak, we switched our operations company-wide to home office and turned our hiring process into <strong>a fully virtual one</strong>. When hiring, we are also open for our new team members starting their work remotely at the time being or adjusting contract start dates accordingly. Stay home and safe!</p>\n<p>InnoGames is Germany’s leading developer and publisher of mobile and online games. The company based in Hamburg is best known for Forge of Empires, Elvenar and Tribal Wars. InnoGames’ complete portfolio encompasses seven live games and several mobile titles in production.</p>\n<p>Born as a hobby, InnoGames today has a team of 400 employees from more than 30 nations who share the passion of creating unique games that players across the globe enjoy for years. In order to further expand our success and to realize new projects, we are constantly looking for young talents, experienced professionals, and creative thinkers.</p>\n',
+    how_to_apply:
+      '<p><a href="https://www.innogames.com/career/detail/job/java-software-developer-core-team/?s=GitHub">https://www.innogames.com/career/detail/job/java-software-developer-core-team/?s=GitHub</a></p>\n',
+    company_logo:
+      "https://jobs.github.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBb1dGIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--c9ccbc9b803859a5d8304b3f7d85cc5a0a6d9202/logo_innogames_devlop.png",
+  },
+  {
+    id: "8d090215-8f42-4b8d-8a27-44cca0394f68",
+    type: "Full Time",
+    url:
+      "https://jobs.github.com/positions/8d090215-8f42-4b8d-8a27-44cca0394f68",
+    created_at: "Sat Jun 27 06:33:39 UTC 2020",
+    company: "Bayforest Technologies",
+    company_url: null,
+    location: "London",
+    title: "Mid-Senior level Software Engineer",
+    description:
+      "<p>Bayforest Technologies, a multi-award winning quantitative investment manager with an outstanding track record, seeks a mid-senior level software engineer.</p>\n<p>Bayforest Technologies is not a conventional hedge fund. We are technologists at heart: our proprietary technology is streaming large amounts of data to identify actionable insights in real time. We are working on the forefront of academia and industry to solve the most challenging and exciting problems in finance.</p>\n<p>We are looking for versatile engineers with strong enthusiasm and a curious nature to drive our technology in order to solve the most challenging problems in finance. You will be involved in our proprietary research  and develop enhanced solutions to deliver them into production. You will be joining a team of developers with previous experience from the likes of market making hedge funds, technology startups, Google, Nokia, Playstation among others.</p>\n<p>Qualifications:</p>\n<p>-Minimum BSc or equivalent in natural sciences, engineering or computer science.</p>\n<p>-Minimum 3 years of experience at  mid-level position or higher as software engineer.</p>\n<p>-Demonstrated proficiency in C++ and one scripting language, preferably Python.</p>\n<p>-Strong awareness of best practices, code standards, building processes, test driven programming and code reviews.</p>\n<p>-Knowledge of algorithm designs.</p>\n<p>-Demonstrable experience of complex software development that was successfully delivered to customers.</p>\n<p>-Familiar with Linux infrastructure.</p>\n<p>-Preferably some knowledge of financial market mechanics.</p>\n<p>A competitive salary is on offer and opportunity to participate in our generous profit sharing in one of the fastest growing and strongly backed systematic hedge funds.</p>\n",
+    how_to_apply:
+      '<p>Email your resume to <a href="mailto:hr@bayforest.ai">hr@bayforest.ai</a></p>\n',
+    company_logo:
+      "https://jobs.github.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBb09GIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--7ce26deb773a04eb9b525d36f04c85bde8684f9a/Image%20logo%20CMYK.jpg",
+  },
+];
---
+++ src/interfaces.ts
@@ -0,0 +1,13 @@
+export interface Job {
+  id: string;
+  type: string;
+  url: string;
+  created_at: string;
+  company: string | null;
+  company_url: string;
+  location: string;
+  title: string;
+  description: string;
+  how_to_apply: string;
+  company_logo: string | null;
+}
