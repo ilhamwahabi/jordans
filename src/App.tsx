8a8949a12faf556f73f00a8bc1e89e11c681b6e3
iwgx <wahabiputra@gmail.com>
2020-06-30 09:45:26 +0700

    Add react-router-dom and init possible views

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./App.css";
+import Home from "./views";
+import Jobs from "./views/jobs";
+import JobsDetail from "./views/jobs/detail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" component={Home} />
+        <Route path="/jobs" component={Jobs} />
+        <Route path="/jobs/:id" component={JobsDetail} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/views/jobs/detail.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function JobsDetail() {
+  return <div>JobsDetail</div>;
+}
+
+export default JobsDetail;
